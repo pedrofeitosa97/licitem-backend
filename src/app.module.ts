1cbee4869374bd89ce8309b544676d7a8f1d920e
Pedro Henrique Feitosa <50965091+pedrofeitosa97@users.noreply.github.com>
2025-09-02 12:43:26 -0300

    feat: created nest routes plug add all remaining libs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MessagesModule } from './messages/messages.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, RoomsModule, MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
