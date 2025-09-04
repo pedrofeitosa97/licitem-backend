77e14e2a5aaae167ec695cfa1d08d510b9a4c93d
Pedro Henrique Feitosa <50965091+pedrofeitosa97@users.noreply.github.com>
2025-09-04 16:34:19 -0300

    fix: fixed firebases service

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -22,7 +22,6 @@ export class FirebaseService {
       auth_provider_x509_cert_url:
         process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
       client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-      universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
     };
 
     if (!admin.apps.length) {
